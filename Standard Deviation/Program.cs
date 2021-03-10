82f6cf71c83d88164f42b92d3acd712454a276bb
Mihai Caramizoiu <fmc1crt@bolton.ac.uk>
2021-03-10 12:18:34 +0000

    Add project files.

---
+++ Standard Deviation/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Standard_Deviation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            getData();
+        }
+        static void getData()
+        {
+            Console.Write("Please enter how many nr are there: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] numbers = new int[n+1];
+            for (int i = 1; i<=n; i++)
+            {
+                Console.Write("{0} ) ", i);
+                numbers[i] = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+    }
+}
