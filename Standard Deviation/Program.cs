085b08dbf8168294c31870b96e7756ba8a42fcfc
Mihai Caramizoiu <fmc1crt@bolton.ac.uk>
2021-03-11 20:18:43 +0000

    Added Comments

--- Standard Deviation/Program.cs
+++ Standard Deviation/Program.cs
@@ -7,8 +7,10 @@ namespace Standard_Deviation
     {
         static void Main(string[] args)
         {
-            int sum = 0; 
+            int sum = 0;
 
+
+            // Getting the inputs and save them into an array.
             Console.Write("Please enter how many nr are there: ");
             int n = Convert.ToInt32(Console.ReadLine());
             int[] numbers = new int[n];
@@ -25,9 +27,14 @@ namespace Standard_Deviation
                 Console.WriteLine(item.ToString());
             }
 
-            // Getting the mean
+            // Step 1: calculate the mean
+
             int mean = Convert.ToInt32(numbers.Average());
             Console.WriteLine("Average is: " + mean);
+
+            // Step 2 Subtract the mean from each value (to calculate the deviation)
+
+
 
             for (int i = 0; i < numbers.Length; i++)
             {
@@ -40,7 +47,7 @@ namespace Standard_Deviation
                 Console.WriteLine(item.ToString());
             }
 
-            // Getting the square for each 
+            // Step 3: Square each of these results
             for (int i = 0; i < numbers.Length; i++)
             {
                 numbers[i] = (int)Math.Pow(numbers[i], 2);
@@ -52,18 +59,23 @@ namespace Standard_Deviation
                 Console.WriteLine(item.ToString());
             }
 
-            for ( int i = 0; i<numbers.Length; i++)
+            // Step 4: Add then all together
+            for (int i = 0; i < numbers.Length; i++)
             {
                 sum = sum + numbers[i];
             }
             Console.WriteLine("The sum is: " + sum);
+
+            // Step 5: Divide by: “number of data items”
+
             int divideBy = sum / numbers.Length;
             Console.WriteLine("The Devide is: " + divideBy);
+
+            //Step 6: Take the square root of the variance to find the standard deviation
             double sdv = Math.Sqrt(divideBy);
             Console.WriteLine("The sdv is: " + sdv);
 
 
         }
-
     }
 }
